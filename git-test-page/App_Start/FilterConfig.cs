2dc62d4c9f19124a6a9805683c46325c89951b15
Jonathan Glodowski <jglodowski2@unl.edu>
2017-09-08 15:03:11 -0500

    Add project files.

---
+++ git-test-page/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace git_test_page
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ git-test-page/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_test_page.MvcApplication" Language="C#" %>
